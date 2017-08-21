8f8169c67c86ffac3ba7d4aecbe844c5e7859f8a
Victoria Rain Coleman <victoriarainc@gmail.com>
2017-08-21 15:59:54 -0400

    created Constructors for Dogs and Humans and made all tests pass

--- main.js
+++ main.js
@@ -7,10 +7,18 @@
 //            /____/
 
 // Dog Constructor & Prototype
-function Dog () {}
+function Dog (hungry, owner, color) {
+  this.status = 'normal';
+  this.hungry = hungry;
+  this.owner = owner;
+  this.color = color;
+}
 
 // Instances of Dog
 // Needed: sadie, moonshine, atticus
+let sadie = new Dog (false, "me", "black");
+let moonshine = new Dog (true);
+let atticus = new Dog (true);
 
 //     __
 //    / /_  __  ______ ___  ____ _____  _____
@@ -19,7 +27,20 @@ function Dog () {}
 // /_/ /_/\__,_/_/ /_/ /_/\__,_/_/ /_/____/
 
 // Human Constructor & Prototype
-function Human () {}
+function Human (cool) {
+  //when a Human pets, set the Dog.status to happy
+  this.pet = function (dog) {
+    dog.status = 'happy';
+  }
+  //When a human feeds, set the Dog.hungry to false
+  this.feed = function (dog) {
+    dog.hungry = false;
+  }
+  //No clue
+  this.cool = cool;
+}
 
 // Instances of Human
 // Needed: mason, julia
+let mason = new Human (false);
+let julia = new Human (true);
